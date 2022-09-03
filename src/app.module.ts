90c79f43322f3d89cd659811cd48fe49ffc05c4b
Max Andrade <maxreisandrade@gmail.com>
2022-09-03 17:29:19 -0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), DatabaseModule],
+})
+export class AppModule {}
